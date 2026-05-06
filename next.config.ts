bb8a6b8a2a8be381fa18e9a8505556420b173a54
Digigit24 <digigit24@gmail.com>
2026-05-06 18:45:21 +0530

    Update next.config.ts to remove undici package
    Removed 'undici' from serverExternalPackages.

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'standalone',
-  serverExternalPackages: ['undici'],
+  
 
   images: {
     unoptimized: true,   // 🔥 THIS IS THE FIX
